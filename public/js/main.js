df2edaf3e5d8cb7ae74dd69ae8c3b5e509f12123
syq1035 <1035394794@qq.com>
2018-10-19 22:24:35 +0800

    Login and send massage

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,111 @@
 //客户端引入socket
 var socket = io();
+
+//点击登录确认键或回车键后，进行登录判断
+$("#namesubmit").click(login);
+$("#name").keyup(function(e){
+    if(e.which == 13){
+        login();
+    }
+})
+
+//监听登录成功事件，隐藏登录层
+socket.on('loginSuccess',(userImg)=>{
+    $(".name").hide();
+    // document.getElementById("userImg").setAttribute("src", userImg);
+    $("#userImg").attr("src", userImg);
+})
+
+//监听登录错误
+socket.on('loginError', ()=>{
+    alert("该用户名被占用，请重新输入！");
+    $("#name").val('');
+    $("#name").focus();
+})
+
+//监听用户显示事件
+socket.on('displayUsers',(usersInfo)=>{
+    displayUsers(usersInfo);
+})
+
+socket.on('systemPrompt', (user)=>{
+    let date = new Date().toTimeString().substr(0,8);
+    $("#chatInfo").append(`<p class='system'>
+        <span>${date}</span>
+        <br>
+        <span>${user.name} ${user.status}了聊天室</span>
+    </p>
+    `)
+    //滚动条总是在最底部
+    $('#chatInfo').scrollTop($('#chatInfo')[0].scrollHeight);
+})
+
+//处理用户显示
+function displayUsers(usersInfo){
+    $("#users").text('');      //每个用户都要重新渲染
+    if(usersInfo.length){
+        $(".onlineMember p").hide();
+    }else{
+        $(".onlineMember p").show();
+    }
+    $(".num").text(usersInfo.length);
+    for(let user of usersInfo){
+        $("#users").append(`<li>
+            <img src=${user.img}>
+            <span>${user.name}</span>
+        </li>
+        `)
+    }
+}
+//处理登录
+function login(){
+    let imgN =Math.ceil(Math.random()*4);
+    //检查昵称输入框是否为空
+    if($("#name").val().trim() != ''){
+        //不为空，则发起一个login事件并将输入的昵称发送到服务器
+        socket.emit('login', {
+            name: $("#name").val().trim(),
+            img: 'image/user' + imgN + '.jpg',
+        })
+    }else{
+        //为空，输入框获得焦点
+        $("#name").focus();
+    }
+}
+
+//------------------------------------------
+
+//点击按钮或回车键发送消息
+$("#sbm").click(function(e){
+    e.preventDefault();   //禁止提交表单自动刷新
+    sendMassage();
+});
+$("#mas").keyup(function(e){
+    if(e.which == 13){
+        sendMassage();
+    }
+})
+//发送消息
+function sendMassage(){
+    if($("#mas").val()==''){
+        alert("请输入内容");
+        $("#mas").focus();
+    }else{
+        socket.emit('sendMassage', {
+            massage: $("#mas").val()
+        })
+    }
+    $("#mas").val('');
+}
+//接收消息并渲染入消息框
+socket.on('receiveMassage', (data)=>{
+    $("#chatInfo").append(`<li class=${data.side}>
+        <img src=${data.img}>
+        <div>
+            <span>${data.name}</span>
+            <p>${data.massage}</p>
+        </div>
+    </li>`)
+    //滚动条总是在最底部
+    $('#chatInfo').scrollTop($('#chatInfo')[0].scrollHeight);
+})
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ let app = express();
 let server = require('http').createServer(app);
 let io = require('socket.io').listen(server);  //引入socket.io模块并绑定到服务器
 
+let users = [];  //存储登录用户昵称
+let usersInfo = [];   //存储用户昵称和头像
+
 // 路由为/默认client静态文件夹
 app.use('/',express.static(__dirname + '/public'));
 
@@ -13,4 +16,43 @@ app.get('/',function(req, res){
 //监听端口
 server.listen(3000,function(){
     console.log("listen on 3000...");
+})
+
+io.on('connection', function(socket){
+    //监听登录
+    socket.on('login', (user)=>{
+        // io.emit('displayUsers', usersInfo);
+
+        //检查昵称是否存在
+        if(users.indexOf(user.name)>-1){
+            socket.emit('loginError');
+        }else{
+            users.push(user.name);  //存储用户昵称
+            usersInfo.push(user);  //存储用户昵称和头像
+            socket.nickName = user.name;
+            socket.img = user.img;
+            socket.emit('loginSuccess', user.img);  //触发当前客户端的登录成功事件
+            io.emit('displayUsers', usersInfo);  //触发所有客服端渲染在线成员事件
+            io.emit('systemPrompt', {
+                name: user.name,
+                status: "进入"
+            });   //系统提示用户进入
+            console.log(`${user.name} coming , ${users.length} user online now.`);   //打印连接人数
+        }
+    })
+    socket.on('sendMassage', (data)=>{
+        //
+        socket.emit('receiveMassage', {
+            name: socket.nickName,
+            img: socket.img,
+            massage: data.massage,
+            side: 'right',
+        })
+        socket.broadcast.emit('receiveMassage', {
+            name: socket.nickName,
+            img: socket.img,
+            massage: data.massage,
+            side: 'left',
+        })
+    })
 })
